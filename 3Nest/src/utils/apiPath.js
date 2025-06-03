3fb61ffd5cbccc96dbf4b5333d9e136608212c62
duc nguyen <duc31200431@gmail.com>
2025-06-03 14:45:28 +0700

    update feref

---
+++ 3Nest/src/utils/apiPath.js
@@ -0,0 +1,19 @@
+export const BASE_URL = "https://7022-2402-800-3dd2-171b-99c3-6a2f-e599-2ec8.ngrok-free.app"
+
+export const API_PATHS = {
+    AUTH: {
+        LOGIN: "/users/login",
+        LOGOUT: "/auth/logout",
+    },
+    USER: {
+        PROFILE: "/user/profile",
+        UPDATE_PROFILE: "/user/update-profile",
+    },
+    PRODUCTS: {
+        CREATE: "/products/create-product",
+        GET_ALL: "/types/",
+        GET_BY_ID: (id) => `/posts/${id}`,
+        UPDATE: (id) => `/posts/${id}/update`,
+        DELETE: (id) => `/posts/${id}/delete`,
+    },
+};
