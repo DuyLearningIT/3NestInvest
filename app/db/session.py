ceeaf3243b0cb18d83c4c9b203d2de92a7a9fd84
Nguyen Quang Duy <nguyenquangduy12112004@gmail.com>
2025-06-10 10:51:27 +0700

    add db and back-end on server

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 # from app.core import DATABASE_URL
 
-DATABASE_URL = 'mysql+pymysql://root:root@localhost:3306/3nestinvest'
+DATABASE_URL = 'mysql+pymysql://admin:NguyenQuangDuy@threenest.c1u22ewui7uw.ap-southeast-2.rds.amazonaws.com:3306/3nestinvest'
 
 engine = create_engine(
 	DATABASE_URL, 
