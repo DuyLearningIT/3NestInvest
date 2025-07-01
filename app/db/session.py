c049c1ec5e5f41e39ecc0f99891b173ec1c2f002
Nguyen Quang Duy <nguyenquangduy12112004@gmail.com>
2025-07-01 09:34:19 +0700

    add db

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 # from app.core import DATABASE_URL
 
-DATABASE_URL = 'mysql+pymysql://root:root@localhost:3306/threenestinvest'
+DATABASE_URL = 'mysql+pymysql://admin:NguyenQuangDuy@dbthreenest.cducasqgky8s.ap-southeast-1.rds.amazonaws.com/dbthreenest'
 
 engine = create_engine(
 	DATABASE_URL, 
